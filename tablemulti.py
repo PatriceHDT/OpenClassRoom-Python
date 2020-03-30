dabdd19a681c36cf1efbdd3660d2de1be22d6c31
Patrice HDT <patrice.hadot@netc.eu>
2020-03-30 16:47:20 +0200

    Ajout table de multiplication

---
+++ tablemulti.py
@@ -0,0 +1,7 @@
+def Multiplication (nb, max):
+    i = 0
+    while i < max: # On se limite à la table des 10
+        print (i +1, "*", nb, "=", (i+1) * nb )
+        i += 1 # On incrémente i
+
+Multiplication (7,12) ;

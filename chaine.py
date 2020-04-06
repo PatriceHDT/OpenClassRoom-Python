f1f2f080299f3c87b49207883ff694c5c9d2ca73
Patrice HDT <patrice.hadot@netc.eu>
2020-04-06 14:13:36 +0200

    ajouts d exercices dans chaine.py

--- chaine.py
+++ chaine.py
@@ -7,7 +7,7 @@ chaine = "NE CRIE PAS SI FORT !"
 print (chaine.lower())
 """
 
-chaine = str()
+""" chaine = str()
 
 while chaine.lower() != "q":
     print("Tapez 'Q' pour quitter...")
@@ -15,4 +15,40 @@ while chaine.lower() != "q":
     print(chaine)
 
 print("Merci !")
+
+"""
+
+"""
+minuscules = str()
+minuscules = "une chaîne en minuscules"
+print(minuscules.upper())
+
+espaces=str()
+espaces="      une chaine     avec des espaces    "
+# on retire les espaces au début et à la fin de la chaîne
+print(espaces.strip())
+
+titre=str()
+titre="introduction"
+print(titre.upper().center(20))
+"""
+
+"""
+prenom = "Paul"
+nom = "Dupont"
+age = 21
+print("je m'appelle {0} {1} et j'ai {2} ans".format(prenom, nom, age))
+print ("Je m'appelle {0} {1} ({3} {0} pour l'administration), et j'ai {2} ans".format(prenom, nom, age, nom.upper()))
+"""
+
+
+# Formatage d'un adresse
+adresse ="""
+{no_rue}, {nom_rue}
+{code_postal} {nom_ville} ({Pays})
+""".format(no_rue=5, nom_rue="rue des Postes", code_postal=75003, nom_ville="Paris", Pays="France") 
+print(adresse)
+
+
+
 

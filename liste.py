2adef3b05def1641cde160825ddd29701b67aee5
Patrice HDT <patrice.hadot@netc.eu>
2020-04-06 16:14:55 +0200

    début chapitre sur les listes

---
+++ liste.py
@@ -0,0 +1,28 @@
+# ma_liste = list() #on crée une liste vide
+# ma_liste =[1,2,3,4,5]
+# print(ma_liste)
+
+# ma_liste=[1,3.5,"trois et demi"]
+# print(ma_liste)
+
+# ma_liste = ['c','f','m']
+# print(ma_liste[0])
+# print(ma_liste[2])
+# ma_liste[1]='Z'
+# print(ma_liste)
+
+# # Parcours de liste
+# ma_liste=['a','b','c','d','e','f','g','h']
+# i=0
+# while i < len(ma_liste):
+#     print(ma_liste[i])
+#     i +=1
+
+# for individu in ma_liste:
+#     print(individu) 
+
+ma_liste=['a','b','c','d','e','f','g','h']
+for elt in enumerate(ma_liste):
+    print(elt)
+
+    

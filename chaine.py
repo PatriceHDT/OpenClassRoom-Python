c2f752ee4a0c05628d26bdea7d55d22ff4817be6
Patrice HDT <patrice.hadot@netc.eu>
2020-04-06 14:32:45 +0200

    fin du chapitre sur les chaines

--- chaine.py
+++ chaine.py
@@ -1,54 +1,67 @@
 # -*-coding:utf-8 -*
 import os
 
-"""
-# Mettre en minuscule une chaîne de caractères
-chaine = "NE CRIE PAS SI FORT !"
-print (chaine.lower())
-"""
-
-""" chaine = str()
-
-while chaine.lower() != "q":
-    print("Tapez 'Q' pour quitter...")
-    chaine = input()
-    print(chaine)
-
-print("Merci !")
-
-"""
-
-"""
-minuscules = str()
-minuscules = "une chaîne en minuscules"
-print(minuscules.upper())
-
-espaces=str()
-espaces="      une chaine     avec des espaces    "
-# on retire les espaces au début et à la fin de la chaîne
-print(espaces.strip())
-
-titre=str()
-titre="introduction"
-print(titre.upper().center(20))
-"""
-
-"""
-prenom = "Paul"
-nom = "Dupont"
-age = 21
-print("je m'appelle {0} {1} et j'ai {2} ans".format(prenom, nom, age))
-print ("Je m'appelle {0} {1} ({3} {0} pour l'administration), et j'ai {2} ans".format(prenom, nom, age, nom.upper()))
-"""
 
 
-# Formatage d'un adresse
-adresse ="""
-{no_rue}, {nom_rue}
-{code_postal} {nom_ville} ({Pays})
-""".format(no_rue=5, nom_rue="rue des Postes", code_postal=75003, nom_ville="Paris", Pays="France") 
-print(adresse)
+# # Mettre en minuscule une chaîne de caractères
+# chaine = "NE CRIE PAS SI FORT !"
+# print (chaine.lower())
+# """
 
 
+
+# """ chaine = str()
+# while chaine.lower() != "q":
+#     print("Tapez 'Q' pour quitter...")
+#     chaine = input()
+#     print(chaine)
+# print("Merci !")
+
+
+# minuscules = str()
+# minuscules = "une chaîne en minuscules"
+# print(minuscules.upper())
+
+# espaces=str()
+# espaces="      une chaine     avec des espaces    "
+# # on retire les espaces au début et à la fin de la chaîne
+# print(espaces.strip())
+# titre=str()
+# titre="introduction"
+# print(titre.upper().center(20))
+
+
+
+# prenom = "Paul"
+# nom = "Dupont"
+# age = 21
+# print("je m'appelle {0} {1} et j'ai {2} ans".format(prenom, nom, age))
+# print ("Je m'appelle {0} {1} ({3} {0} pour l'administration), et j'ai {2} ans".format(prenom, nom, age, nom.upper()))
+
+
+
+# # Formatage d'un adresse
+# adresse ="""
+# {no_rue}, {nom_rue}
+# {code_postal} {nom_ville} ({Pays})
+# """.format(no_rue=5, nom_rue="rue des Postes", code_postal=75003, nom_ville="Paris", Pays="France") 
+# print(adresse)
+
+
+# # Concatenation et conversion int vers stt
+# age = 21
+# print("j'ai " + str(age) + " ans")
+
+
+# chaine = "Salut !"
+# i = 0
+# while i < len(chaine):
+#     print(chaine[i])
+#     i += 1
+
+
+# presentation = "Salut !"
+# print(presentation[0:2])
+# print(presentation[2:len(presentation)])
 
 

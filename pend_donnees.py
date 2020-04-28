3bdc73a88cb41d32d936ef98a66afcd6cc6ef4c3
Patrice HDT <patrice.hadot@netc.eu>
2020-04-28 14:52:01 +0200

    ajout liste des mots dabs pend-donnees.py

---
+++ pend_donnees.py
@@ -0,0 +1,11 @@
+mots = {}
+mots[1]='ABATTAGE'
+mots[2]='ABOMINER'
+mots[3]='ABREUVEE'
+mots[4]='ACHETEUR'
+mots[5]='BAGUETTE'
+mots[6]='BANQUISE'
+mots[7]='BOUTIQUE'
+mots[8]='BRONZEES'
+mots[9]='CAHUTTES'
+mots[10]='CAMELOTE'
